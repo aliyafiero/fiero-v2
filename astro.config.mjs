d4a5a18a82316ad4efaf0bde3f1dc0ec2e7d915a
aliyafiero <158611715+aliyafiero@users.noreply.github.com>
2026-09-15 14:09:19 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  site: 'https://aliyafiero.github.io',
+  base: '/fiero-v2'
 });
